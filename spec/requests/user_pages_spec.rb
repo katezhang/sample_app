93f5a800a2e1fadf961ae3c6ef24cd2082dcfb88
klz0123@gmail.com <kate@tapjoy.com>
2012-12-05 14:31:46 -0800

    finish layout and routes

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -5,4 +5,5 @@ describe "User pages" do
     before { visit signup_path }
     it { should have_selector('h1', text: 'Sign up') }
     it { should have_selector('title', text: full_title('Sign up')) }
-end end
+end 
+end
